acbc455a3b9502dbd050452bffc7a863417fddf2
abdfal <naufaldec07@gmail.com>
2023-09-01 14:48:21 +0700

    add activity

--- app/src/main/java/com/nofal/staytune/SecondActivity.kt
+++ app/src/main/java/com/nofal/staytune/SecondActivity.kt
@@ -1,11 +1,18 @@
 package com.nofal.staytune
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val btnNext = findViewById<Button>(R.id.button3)
+        btnNext.setOnClickListener(){
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
